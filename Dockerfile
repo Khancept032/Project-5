400c73ff160c1d50311476f2a2c68e0ab29771e3
Nathan Taylor <nathanhtaylor@att.net>
2019-04-02 17:02:29 -0500

    init commit

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM python:3.7
 COPY . /usr/src/app
 WORKDIR /usr/src/app
 RUN pip install -r requirements.txt
-CMD ["Python", "app.py"]
+CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,37 +1,38 @@
 from flask import Flask, request
-from redis import Redis
-import jason
+# from redis import Redis
+# import jason
 
 app = Flask(__name__)
-app.redis = Redis("host="redis", port=6379)
+#app.redis = Redis("host="redis", port=6379)
 
 
 @app.route("/")
 def index():
-    return "it works"
+    return "it works again"
 
-@app.route('/kv-retrieve/<id>', methods=["GET"])
-def get_post(id):
-    # get from database
-    post = app.redis.get(id)
-    if post:
-        data = json.dumps(postdecode('utf-8'))
-    else:
-        data = json.dumps({})
-    return data
+# commented out for simplicity during setup
 
-@app.route("/kv-record/<id>", methods=["POST"])
-def create_post(id)
-    # get the post from the POSTed data
-    data = request.data.decode("utf-8")
-    post = json.loads(data)
-
-    # write to database
-    app.redis.set(id, json.dumps(post))
-
-    return "True"
+# @app.route('/kv-retrieve/<id>', methods=["GET"])
+# def get_post(id):
+#     # get from database
+#     post = app.redis.get(id)
+#     if post:
+#         data = json.dumps(postdecode('utf-8'))
+#     else:
+#         data = json.dumps({})
+#     return data
+#
+# @app.route("/kv-record/<id>", methods=["POST"])
+# def create_post(id)
+#     # get the post from the POSTed data
+#     data = request.data.decode("utf-8")
+#     post = json.loads(data)
+#
+#     # write to database
+#     app.redis.set(id, json.dumps(post))
+#
+#     return "True"
 
 if __name__ == '__main__':
     app.debug = True
     app.run("0.0.0.0")
-
