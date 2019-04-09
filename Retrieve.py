f60832b402fed71653d2d440717367328d074ea0
Abdul Khan <abdul_khan@tamu.edu>
2019-04-08 20:28:32 -0500

    if main

--- Retrieve.py
+++ Retrieve.py
@@ -8,3 +8,18 @@ app = Flask(__name__)
 app.redis = Redis(host="redis", port=6379)
 
 @app.route('/kv-retrieve/<id>', methods=["GET"])
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run("0.0.0.0")
