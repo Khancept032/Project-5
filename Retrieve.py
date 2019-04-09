39b85ed862cedc55506882996e3048a2014303d1
Abdul Khan <abdul_khan@tamu.edu>
2019-04-08 21:04:18 -0500

    Troubleshooting Retrieve Route

--- Retrieve.py
+++ Retrieve.py
@@ -1,22 +1,23 @@
 #Using this file to do some testing for retrieve
 
-from flask import flask, request
+from flask import flask, request, jsonify
 from redis import Redis
 import json
 
+
 app = Flask(__name__)
-app.redis = Redis(host="redis", port=6379)
-
-@app.route('/kv-retrieve/<id>', methods=["GET"])
 
 
+@app.route('/kv-retrieve/<string:key>')
+def retrieve(key):
+    try;
+        if redis.exits(key):
+                return json.dumps({"input": "retrieve-value", "output": redis.get(key)})
+        else:
+                return json.dumps({"input": "retrieve-value", "output" False, "error": "Not able to update value, the key does not exist."})
 
-
-
-
-
-
-
+    except Exception as error:
+        return json.dumps({"output": False, "error": str(error)})
 
 
 
