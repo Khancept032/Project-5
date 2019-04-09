dedd272d6fb0ce581d3026e07775b1a8058877e9
Abdul Khan <abdul_khan@tamu.edu>
2019-04-08 20:17:37 -0500

    retrieve start

--- Retrieve.py
+++ Retrieve.py
@@ -1,1 +1,10 @@
-#Using this file to do some testing retrieve
+#Using this file to do some testing for retrieve
+
+from flask import flask, request
+from redis import Redis
+import json
+
+app = Flask(__name__)
+app.redis = Redis(host="redis", port=6379)
+
+@app.route('/kv-retrieve/<id>', methods=["GET"])
