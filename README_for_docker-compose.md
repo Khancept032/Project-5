20b1937707343062f542c6dab7f73d96c18d2ea5
Nathan Taylor <nathanhtaylor@att.net>
2019-04-23 18:55:40 -0500

    finished transition to ports 5000/5001, removed vestigial comments

--- README_for_docker-compose.md
+++ README_for_docker-compose.md
@@ -4,12 +4,12 @@ The file in this directory called "docker-compose.yml" will make two containers,
 ## Veiwing API
 ### Docker Desktop:
 1. Follow steps in *Use*
-2. *Heavy will be on http://localhost:5001 and Light will be on http://localhost:5002*
+2. *Heavy will be on http://localhost:5000 and Light will be on http://localhost:5001*
 
 ### Docker Toolbox:
 1. Follow steps in *Use*
 2. run `docker-machine ip`
-3. take IP addess from step 2 and plug it in to browser followed by port 5001 or 5002. e.g.: *http://xxx.xxx.xxx.xxx:5001 for heavy and http://xxx.xxx.xxx.xxx:5002 for light*
+3. take IP addess from step 2 and plug it in to browser followed by port 5001 or 5002. e.g.: *http://xxx.xxx.xxx.xxx:5000 for heavy and http://xxx.xxx.xxx.xxx:5001 for light*
 
 
 ## Use
--- tcm.py
+++ tcm.py
@@ -1,25 +1,3 @@
-# #!/usr/bin/env python
-# # -*- coding: utf-8 -*-
-# usage = """Usage:
-#         test.py -h
-#         test.py md5 <input>
-#         test.py factorial <input>
-# """
-#
-# from docopt import docopt
-# import requests
-#
-# args = docopt(usage)
-#
-# if args['md5']==True:
-#     r = requests.get("http://localhost:5000/md5/"+ args["<input>"])
-#     data = r.json()
-#     print(data['output'])
-# if args['factorial']==True:
-#     r = requests.get("http://localhost:5000/factorial/"+ args["<input>"])
-#     data = r.json()
-#     print(data['output'])
-
 usage = '''
 
 API CLI Texas Chainsaw Managers
@@ -52,27 +30,27 @@ if args['is_prime']:
     print(data.text)
     data = data.json()
     print(data['output'])
-    
+
 if args['md5']:
     data = requests.get('http://localhost:5001/md5'+'/'+args['<input>'])
     print(data.text)
     data = data.json()
     print(data['output'])
-    
+
 if args['fibonacci']:
     data = requests.get('http://localhost:5000/fibonacci'+'/'+args['<input>'])
     print(data.text)
     data = data.json()
     print(data['output'])
-    
+
 if args['factorial']:
     data = requests.get('http://localhost:5000/factorial'+'/'+args['<input>'])
     print(data.text)
     data = data.json()
-    print(data['output']) 
+    print(data['output'])
 
 if args['kv-retrieve']:
     data = requests.get('http://localhost:5000/kv-retrieve'+'/'+args['<input>'])
     print(data.txt)
     data = data.json()
-    print(data['output'])   
+    print(data['output'])
