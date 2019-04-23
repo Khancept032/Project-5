83dc56db62c56846189e9875b7dcd44d43587428
Eddie Alvarado <eddie.alvarado@tamu.edu>
2019-04-23 12:29:39 -0500

    Update tcm.py

--- tcm.py
+++ tcm.py
@@ -42,37 +42,37 @@ from docopt import docopt
 import requests
 
 if args['banana']:
-    data = requests.get('http://192.168.99.100:5000/banana')
+    data = requests.get('http://localhost:5000/banana')
     print(data.text)
     data = data.json()
     print(data['is_banana'])
 
 if args['is_prime']:
-    data = requests.get('http://192.168.99.100:5000/is_prime'+'/'+args['<input>'])
+    data = requests.get('http://localhost:5000/is_prime'+'/'+args['<input>'])
     print(data.text)
     data = data.json()
     print(data['output'])
     
 if args['md5']:
-    data = requests.get('http://192.168.99.100:5001/md5'+'/'+args['<input>'])
+    data = requests.get('http://localhost:5001/md5'+'/'+args['<input>'])
     print(data.text)
     data = data.json()
     print(data['output'])
     
 if args['fibonacci']:
-    data = requests.get('http://192.168.99.100:5000/fibonacci'+'/'+args['<input>'])
+    data = requests.get('http://localhost:5000/fibonacci'+'/'+args['<input>'])
     print(data.text)
     data = data.json()
     print(data['output'])
     
 if args['factorial']:
-    data = requests.get('http://192.168.99.100:5000/factorial'+'/'+args['<input>'])
+    data = requests.get('http://localhost:5000/factorial'+'/'+args['<input>'])
     print(data.text)
     data = data.json()
     print(data['output']) 
 
 if args['kv-retrieve']:
-    data = requests.get('http://192.168.99.100:5000/kv-retrieve'+'/'+args['<input>'])
+    data = requests.get('http://localhost:5000/kv-retrieve'+'/'+args['<input>'])
     print(data.txt)
     data = data.json()
     print(data['output'])   
