42f08e01ca5d23efe9db0b7e2351af44f15382c2
Nathan Taylor <nathanhtaylor@att.net>
2019-04-23 20:07:49 -0500

    made CLI user friendly and added remaining commands

--- tcm.py
+++ tcm.py
@@ -4,11 +4,12 @@ API CLI Texas Chainsaw Managers
 
 Usage:
     tcm.py banana
-    tcm.py is_prime <input>
-    tcm.py fibonacci <input>
-    tcm.py kv-retrieve <input>
-    tcm.py factorial <input>
+    tcm.py is-prime <input>
     tcm.py md5 <input>
+    tcm.py fibonacci <input>
+    tcm.py factorial <input>
+    tcm.py kv-record <input>
+    tcm.py kv-retrieve <input>
 
 '''
 
@@ -19,38 +20,43 @@ args = docopt(usage)
 from docopt import docopt
 import requests
 
+def human(com, arg, res):
+    res = res.json()
+    print('\n\tCLI command: \t' + com + ' ' + str(arg))
+    print('\tAPI response: \t' + str(res['output']) + '\n')
+    return
+
 if args['banana']:
     data = requests.get('http://localhost:5000/banana')
-    print(data.text)
-    data = data.json()
-    print(data['is_banana'])
+    # print(data.text)      # for debugging
+    human('banana', '', data)
 
-if args['is_prime']:
-    data = requests.get('http://localhost:5000/is_prime'+'/'+args['<input>'])
-    print(data.text)
-    data = data.json()
-    print(data['output'])
+if args['is-prime']:
+    data = requests.get('http://localhost:5000/is_prime/' + args['<input>'])
+    # print(data.text)      # for debugging
+    human('is-prime', args['<input>'], data)
 
 if args['md5']:
-    data = requests.get('http://localhost:5001/md5'+'/'+args['<input>'])
-    print(data.text)
-    data = data.json()
-    print(data['output'])
+    data = requests.get('http://localhost:5000/md5/' + args['<input>'])
+    # print(data.text)      # for debugging
+    human('md5', args['<input>'], data)
 
 if args['fibonacci']:
-    data = requests.get('http://localhost:5000/fibonacci'+'/'+args['<input>'])
-    print(data.text)
-    data = data.json()
-    print(data['output'])
+    data = requests.get('http://localhost:5000/fibonacci/' + args['<input>'])
+    # print(data.text)      # for debugging
+    human('fibonacci', args['<input>'], data)
 
 if args['factorial']:
-    data = requests.get('http://localhost:5000/factorial'+'/'+args['<input>'])
-    print(data.text)
-    data = data.json()
-    print(data['output'])
+    data = requests.get('http://localhost:5000/factorial/' + args['<input>'])
+    # print(data.text)      # for debugging
+    human('factorial', args['<input>'], data)
+
+if args['kv-record']:
+    data = requests.get('http://localhost:5000/kv-record/' + args['<input>'])
+    # print(data.text)      # for debugging
+    human('kv-record', args['<input>'], data)
 
 if args['kv-retrieve']:
-    data = requests.get('http://localhost:5000/kv-retrieve'+'/'+args['<input>'])
-    print(data.txt)
-    data = data.json()
-    print(data['output'])
+    data = requests.get('http://localhost:5001/kv-retrieve/' + args['<input>'])
+    # print(data.text)      # for debugging
+    human('kv-retrieve', args['<input>'], data)
